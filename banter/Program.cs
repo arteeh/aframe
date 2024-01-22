1f74bef42fd41178720ba5b234568c3c4804eaeb
Maarten de Jong <35239587+arteeh@users.noreply.github.com>
2024-01-22 21:06:24 +0100

    asdf

---
+++ banter/Program.cs
@@ -0,0 +1,19 @@
+using banter.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+var app = builder.Build();
+
+app.Urls.Add("http://+:80");
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
