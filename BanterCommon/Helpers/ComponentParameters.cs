316f1376e9d72b30778616469f93336f3da39837
Maarten de Jong <me@arteeh.com>
2024-01-27 00:48:54 +0100

    Greenhouse world; BanterCommon project

---
+++ BanterCommon/Helpers/ComponentParameters.cs
@@ -0,0 +1,14 @@
+﻿namespace BanterCommon;
+
+public static class ComponentParameters
+{
+    public static Dictionary<string, object> Get(object obj)
+    {
+        Dictionary<string, object> parameters = new Dictionary<string,object>();
+        foreach (var property in obj.GetType().GetProperties())
+        {
+            parameters.Add(property.Name, property.GetValue(obj));
+        }
+        return parameters;
+    }
+}
