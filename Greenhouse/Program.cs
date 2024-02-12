1814d52e894b0edcbf7d5b1f2359ce4b1b23e07f
Maarten de Jong <maarten.de.jong@batenburg.nl>
2024-02-12 11:33:42 +0100

    wowie

--- Greenhouse/Program.cs
+++ Greenhouse/Program.cs
@@ -1,5 +1,5 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<Greenhouse.Greenhouse>("#app");
+builder.RootComponents.Add<Greenhouse.Greenhouse>("#greenhouse");
 await builder.Build().RunAsync();
