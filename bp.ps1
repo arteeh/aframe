e1d5ba89820c82bcdd83b5dcca5f87ac12dea79a
Maarten de Jong <maarten.de.jong@batenburg.nl>
2024-03-10 22:16:23 +0100

    cleanup

--- bp.ps1
+++
@@ -1,3 +0,0 @@
-docker buildx bake --push --set *.platform=linux/amd64,linux/arm64/v8
-docker push arteeh/banter-hub:latest
-docker push arteeh/banter-greenhouse:latest
--- bp.sh
+++
@@ -1,4 +0,0 @@
-#/bin/sh
-docker buildx bake --push --set *.platform=linux/amd64,linux/arm64/v8
-docker push arteeh/banter-hub:latest
-docker push arteeh/banter-greenhouse:latest
