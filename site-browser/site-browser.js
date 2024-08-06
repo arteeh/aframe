edd943449d4ec28b7bcc256a9a9e0b2949f35318
Maarten de Jong <me@arteeh.com>
2024-08-07 00:03:58 +0200

    woowoo

--- site-browser/site-browser.js
+++ site-browser/site-browser.js
@@ -1,1 +1,26 @@
-console.log("hello i am site browser woowoo")
+console.log("Site Browser version 1.0")
+
+class SiteBrowser extends HTMLElement
+{
+	constructor() {
+		// Always call super first in constructor
+		super();
+	}
+	
+	connectedCallback()
+	{
+		console.log("Custom element added to page.");
+	}
+	
+	disconnectedCallback()
+	{
+		console.log("Custom element removed from page.");
+	}
+	
+	adoptedCallback()
+	{
+		console.log("Custom element moved to new page.");
+	}
+}
+
+customElements.define("site-browser", SiteBrowser);
