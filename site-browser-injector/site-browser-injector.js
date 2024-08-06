b90bed64f314c1aafce9c28a31c7ddfcadd56e78
Maarten de Jong <me@arteeh.com>
2024-08-06 23:34:32 +0200

    finish initial script

--- site-browser-injector/site-browser-injector.js
+++ site-browser-injector/site-browser-injector.js
@@ -1,6 +1,7 @@
-var siteBrowserScript = '<script src="https://aframe.io/releases/1.6.0/aframe.js"></script>'
+var siteBrowserScript = '<script src="https://raw.githubusercontent.com/arteeh/aframe/master/site-browser/site-browser.js"></script>'
 var siteBrowserHtml = "<site-browser></site-browser>"
 
+var head = document.head.insertAdjacentHTML("beforeend", siteBrowserScript);
 
 var scenes = document.body.querySelectorAll("a-scene");
 for(var i = 0; i < scenes.length; i++)
